3e7f59580bbb6de4b16e17a99c2ffecffb26cb19
Marco Ramos <mfilipe@gmail.com>
2020-05-01 18:42:40 -0400

    ageAdjusted

--- js/math.js
+++ js/math.js
@@ -3,7 +3,7 @@ const minsPerHour = 60;
 const hoursPerDay = 24;
 const daysPerWeek = 7;
 const weeksPerYear = 52;
-const myAge = 42;
+const myAge = 43;
 
 // const secondsPerDay = secsPerMin * minsPerHour * hoursPerDay;
 const secondsAlive = secsPerMin * minsPerHour * hoursPerDay * daysPerWeek * weeksPerYear * myAge;
